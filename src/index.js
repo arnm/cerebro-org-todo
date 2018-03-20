a5e62ea4616df1d06affffa73b8de20d4a71d873
Alexei Nunez <alexeirnunez@gmail.com>
2018-03-19 23:58:52 -0500

    Revert "delete a todo item"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
-import TodoManager from './components/TodoManager';
+const org = require('org-mode-parser');
+const fs = require('fs');
+
+import TodoNode from './components/TodoNode';
+
+function makelist(orgFile) {
+  return new Promise((res, rej) => {
+    try {
+      org.makelist(orgFile, (nodes) => {
+        res(nodes);
+      });
+    } catch (e) {
+      rej(e);
+    }
+  });
+}
+
+function read(filename) {
+  return new Promise((res, rej) => {
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) rej(err);
+      res(data);
+    });
+  });
+}
+
+function write(filename, data) {
+  return new Promise((res, rej) => {
+    fs.writeFile(filename, data, 'utf8', (err) => {
+      if (err) rej(err);
+      res();
+    });
+  });
+}
 
 export const keyword = 'todos';
 export const name = 'Manage Org Todos...';
@@ -7,9 +40,24 @@ export const fn = async ({ term, display, settings }) => {
 
   if (!term.startsWith(keyword)) return;
 
-  display({
-    title: `for ${settings.todoFile}`,
-    getPreview: () => <TodoManager todoFile={settings.todoFile} />
+  const nodes = (await makelist(settings.todoFile));
+
+  const onNodeSave = async (index, updatedNode) => {
+    nodes[index] = updatedNode;
+
+    const updatedContent = nodes.map(node => node.toOrgString().trim()).join('\n\n');
+    await write(settings.todoFile, updatedContent);
+  };
+
+  nodes.forEach((node, index) => {
+    if (node.todo === null) return;
+
+    console.log(`looping: ${index}`);
+    display({
+      title: node.headline,
+      // key for react rendering, index for node saving logic
+      getPreview: () => <TodoNode node={node} key={index} index={index} onNodeSave={onNodeSave}/>
+    });
   });
 
 };
